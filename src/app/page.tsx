7d0232f03c2fdac442ca48aa1984a46b24103406
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-05-13 12:20:35 +0800

    chore: add brand colors & fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center bg-white px-16 py-32">
-        <h1 className="text-5xl font-bold tracking-tight text-black sm:text-[5rem]">
+      <main className="flex min-h-screen w-full max-w-3xl items-center justify-center bg-white px-16 py-32">
+        <h1 className="text-5xl font-bold text-black sm:text-[5rem]">
           Hello World
         </h1>
       </main>
