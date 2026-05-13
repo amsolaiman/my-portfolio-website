e74960ad49ce914f943490ddcd9734ae6fb50eab
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2026-05-13 08:58:48 +0800

    chore: add env schema & validation (#10)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
 
+import { env, validateEnvClient } from './env.schema';
+
+const nextEnv = {
+  NEXT_PUBLIC_HOST_URL: env.HOST_URL,
+};
+
+validateEnvClient(nextEnv);
+
 const nextConfig: NextConfig = {
   /* config options here */
+  env: nextEnv,
 };
 
 export default nextConfig;
