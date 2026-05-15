42c1ff85d8901b8bc106d43cfb0bf6061efaee8e
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2026-05-15 13:08:14 +0800

    feat: add home about section & hide scrollbar in globals.css (#19)

---
+++ src/sections/home/home-about.tsx
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------
+
+export default function HomeAbout() {
+  return (
+    <div className="w-full px-12 pt-32">
+      <h3 className="max-w-7xl font-sans text-5xl [&_span]:opacity-50">
+        Web developer<span>+</span>designer based in the Philippines with 3+
+        years of experience. The focus:
+        <span>
+          &nbsp;fast & clear frontend experiences. No complexity.&nbsp;
+        </span>
+        For established brands, startups and everthing in between.
+      </h3>
+
+      <div className="mt-32 grid grid-cols-2">
+        <p className="text-secondary text-xs">/ Who I am</p>
+      </div>
+    </div>
+  );
+}
