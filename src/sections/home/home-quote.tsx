4ac85cedefa2f9fcbd8b0619079307d3d415d06d
Moiz--Access <abdul.solaiman@accessgroup.net.au>
2026-05-18 15:24:48 +0800

    feat: add home quote section (#28)

---
+++ src/sections/home/home-quote.tsx
@@ -0,0 +1,17 @@
+// ----------------------------------------------------------------------
+
+export default function HomeQuote() {
+  return (
+    <div className="w-full pt-32 pr-12 pl-44">
+      <h3 className="max-w-230 font-sans text-5xl [&_span]:opacity-50">
+        The beginning of wisdom is the statement&nbsp;
+        <span>I do not know</span>. The person who cannot make that statement is
+        one who will never <span>learn</span> anything.
+      </h3>
+
+      <p className="[&_span]:text-secondary text-xs">
+        / Keith R.A. DeCandido <span>/ Cycle of Hatred</span>
+      </p>
+    </div>
+  );
+}
