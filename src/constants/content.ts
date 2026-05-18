ec818025e0c5e234fc4367158852fec44f5dd543
Moiz--Access <abdul.solaiman@accessgroup.net.au>
2026-05-18 12:50:02 +0800

    feat: rename home-skills --> home-services & update contents & design (#22)

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -1,11 +1,11 @@
 // ----------------------------------------------------------------------
 
-export const SKILL_SET = {
-  Langs: ['Typescript', 'Java', 'C#'],
-  Front: ['React', 'Next.js', 'React Native', 'Vue', 'Quasar'],
-  Style: ['Tailwind', 'Sass', 'MUI', 'Shadcn'],
-  Back: ['Node.js', 'Express', 'SQL', 'Prisma', 'GraphQL'],
-  Tools: ['Figma', 'Git', 'Builder.io', 'Motion', 'Adobe CC'],
-};
+export const SKILL_SET = [
+  'Next.js',
+  'Typescript',
+  'React Native',
+  'Figma',
+  'Tailwind',
+];
 
 export const DEFAULT_TIMEZONE = 'Asia/Manila';
