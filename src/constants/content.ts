bdd9d98189ee14f9f1d7a6379ab9269b83d08069
Moiz--Access <abdul.solaiman@accessgroup.net.au>
2026-05-19 15:25:46 +0800

    refactor: apply global content to use-business-time hook logic (#35)

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -8,8 +8,8 @@ export const SKILL_SET = [
   'Tailwind',
 ];
 
-export const BASE_CITY = 'Marawi';
-export const BASE_COUNTRY = 'Philippines';
-export const BASE_LOCATION = `${BASE_CITY}, ${BASE_COUNTRY}`;
+export const FALLBACK_BASE_CITY = 'Marawi';
+export const FALLBACK_BASE_COUNTRY = 'Philippines';
+export const FALLBACK_BASE_LOCATION = `${FALLBACK_BASE_CITY}, ${FALLBACK_BASE_COUNTRY}`;
 
 export const DEFAULT_TIMEZONE = 'Asia/Manila';
