a8dee24b8c27415a64963ecacd045536340dd345
Moiz--Access <abdul.solaiman@accessgroup.net.au>
2026-05-19 13:10:10 +0800

    feat: add global content schema to sanity studio (#31)
    * feat: add global content schema to sanity studio
    
    * feat: add skills & portraitImage fields
    
    * fix: exclude sanity studio from globals.css

---
+++ src/_sanity/utils/content.ts
@@ -0,0 +1,35 @@
+// types
+import { IContent } from '@/types/data';
+
+//
+import { client } from '../client';
+
+// ----------------------------------------------------------------------
+
+export async function getGlobalContentData(): Promise<IContent> {
+  const query = `*[_id == "global-content"][0]{
+    _id,
+    title,
+    description,
+    email,
+    "resume": resume.asset->url,
+    socialLink[] {
+      _key,
+      label,
+      link
+    },
+    businessDays,
+    businessHours,
+    skills,
+    "portraitImage": {
+      "src": portraitImage.asset->url,
+      "alt": portraitImage.alt
+    },
+    city,
+    country,
+  }`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
