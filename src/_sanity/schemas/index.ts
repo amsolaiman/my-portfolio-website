8be55cf4802314e1f7d4a8905efd8fa1a865ef07
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-05-20 10:06:49 +0800

    chore: add project schema to sanity

--- src/_sanity/schemas/index.ts
+++ src/_sanity/schemas/index.ts
@@ -1,9 +1,10 @@
 //
 import content from './content';
 import experience from './experience';
+import projects from './projects';
 
 // ----------------------------------------------------------------------
 
-const schemas = [content, experience];
+const schemas = [content, experience, projects];
 
 export default schemas;
