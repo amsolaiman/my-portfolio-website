5f8adc0b757630559015a4efd7f2163986c08ab9
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-05-20 08:52:34 +0800

    refactor: restructure home-view & display experience content to home career

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import './globals.css';
 import { GlobalContentProvider } from '@/contexts/use-global-content';
 // utils
 import { getGlobalContentData } from '@/_sanity/utils/content';
-import { getExperienceData } from '@/_sanity/utils/experience';
 // constants
 import {
   FALLBACK_METADATA_DESCRIPTION,
@@ -69,10 +68,6 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const globalContent = await getGlobalContentData();
-
-  // TODO: remove when data is applied
-  const experience = await getExperienceData();
-  console.log(experience);
 
   return (
     <html lang="en">
--- src/sections/home/view/home-view.tsx
+++ src/sections/home/view/home-view.tsx
@@ -1,48 +1,18 @@
-'use client';
-
-import { useEffect, useRef } from 'react';
-import Lenis from 'lenis';
-
-//
+// sections
 import HomeAbout from '../home-about';
 import HomeCareer from '../home-career';
 import HomeFooter from '../home-footer';
 import HomeHero from '../home-hero';
 import HomeQuote from '../home-quote';
 
+//
+import HomeClient from './home-client';
+
 // ----------------------------------------------------------------------
 
 export default function HomeView() {
-  const containerRef = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-    const container = containerRef.current;
-    if (!container) return;
-
-    const lenis = new Lenis({
-      wrapper: container,
-      content: container.firstElementChild as HTMLElement,
-      eventsTarget: container,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    const rafId = requestAnimationFrame(raf);
-
-    return () => {
-      cancelAnimationFrame(rafId);
-      lenis.destroy();
-    };
-  }, []);
-
   return (
-    <section
-      ref={containerRef}
-      className="bg-background relative flex h-full w-screen grow flex-col overflow-y-scroll pr-32"
-    >
+    <HomeClient>
       <HomeHero />
 
       <HomeAbout />
@@ -52,6 +22,6 @@ export default function HomeView() {
       <HomeQuote />
 
       <HomeFooter />
-    </section>
+    </HomeClient>
   );
 }
