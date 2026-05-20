4b81089afc5d5ac1929dea84fbbeb975d4d1cfe0
Moiz--Access <abdul.solaiman@accessgroup.net.au>
2026-05-20 08:38:12 +0800

    chore: add experience schema to sanity (#37)

--- src/types/data.ts
+++ src/types/data.ts
@@ -2,6 +2,32 @@ import { ChannelSocialLinkType } from './constant';
 
 // ----------------------------------------------------------------------
 
+type ExperienceType =
+  | 'full-time'
+  | 'part-time'
+  | 'self-employed'
+  | 'freelance'
+  | 'contract'
+  | 'internship'
+  | 'apprenticeship'
+  | 'seasonal'
+  | 'student';
+
+export interface IExperience {
+  _id: string;
+  title: string;
+  type: ExperienceType;
+  isCurrent: boolean;
+  startDate: Date;
+  endDate?: Date | null;
+  employer: {
+    name: string;
+    link?: string;
+  };
+}
+
+// ----------------------------------------------------------------------
+
 export interface IContent {
   _id: string;
   title: string;
