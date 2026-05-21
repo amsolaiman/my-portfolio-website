97661d82cc98cb35971e08fa113a0d6eabf366f2
Moiz--Access <abdul.solaiman@accessgroup.net.au>
2026-05-21 10:48:39 +0800

    feat: add cursor effect component (#47)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+// components
+import CursorEffect from '@/components/cursor-effect';
 // sections
 import ContactView from '@/sections/contact/view';
 import HomeView from '@/sections/home/view';
@@ -8,6 +10,8 @@ import ProjectView from '@/sections/project/view';
 export default function App() {
   return (
     <main className="relative h-svh w-screen overflow-hidden xl:h-screen">
+      <CursorEffect />
+
       <HomeView />
 
       <ProjectView />
