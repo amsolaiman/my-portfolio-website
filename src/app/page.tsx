80394bdae83eb109e5eebae7108797e639676c69
Moiz--Access <abdul.solaiman@accessgroup.net.au>
2026-05-22 13:30:46 +0800

    feat: add loading page & components (#55)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // components
 import CursorEffect from '@/components/cursor-effect';
+import { LoadingScreen } from '@/components/loading-screen';
 // sections
 import ContactView from '@/sections/contact/view';
 import HomeView from '@/sections/home/view';
@@ -10,6 +11,8 @@ import ProjectView from '@/sections/project/view';
 export default function App() {
   return (
     <main className="relative h-svh w-screen overflow-hidden xl:h-screen">
+      <LoadingScreen />
+
       <CursorEffect />
 
       <HomeView />
