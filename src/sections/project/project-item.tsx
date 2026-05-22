7194dcbbd9a8f9f6addbeedc49f5f232a8e64873
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-05-22 08:37:31 +0800

    feat: add project details modal

--- src/sections/project/project-item.tsx
+++ src/sections/project/project-item.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import { useState } from 'react';
 
 // utils
 import { cn } from '@/utils/tw-merge';
@@ -11,6 +12,9 @@ import { IProject } from '@/types/data';
 // constants
 import { FALLBACK_IMAGE_URL } from '@/constants/content';
 
+//
+import ProjectModal from './project-modal';
+
 // ----------------------------------------------------------------------
 
 type Props = {
@@ -19,35 +23,44 @@ type Props = {
 };
 
 export default function ProjectItem({ project, index }: Props) {
+  const [open, setOpen] = useState<boolean>(false);
+
   const upMd = useBreakpoint('up', 'md');
 
   return (
-    <div
-      className={cn(
-        'flex w-full flex-col md:w-72',
-        upMd
-          ? {
-              'self-start': project.align === 'start',
-              'self-center': project.align === 'center',
-              'self-end': project.align === 'end',
-            }
-          : 'self-start'
-      )}
-    >
-      <p className="mb-2 text-xs">({String(index).padStart(2, '0')})</p>
+    <>
+      <div
+        className={cn(
+          'flex w-full flex-col md:w-72',
+          upMd
+            ? {
+                'self-start': project.align === 'start',
+                'self-center': project.align === 'center',
+                'self-end': project.align === 'end',
+              }
+            : 'self-start'
+        )}
+      >
+        <p className="mb-2 text-xs">({String(index).padStart(2, '0')})</p>
 
-      <div className="relative aspect-3/4 w-full">
-        <Image
-          src={project.posterImage.src || FALLBACK_IMAGE_URL}
-          alt={project.posterImage.alt || project.name}
-          fill
-          priority
-          draggable={false}
-          className="object-cover object-center"
-        />
+        <button
+          onClick={() => setOpen(true)}
+          className="relative aspect-3/4 w-full cursor-pointer"
+        >
+          <Image
+            src={project.posterImage.src || FALLBACK_IMAGE_URL}
+            alt={project.posterImage.alt || project.name}
+            fill
+            priority
+            draggable={false}
+            className="object-cover object-center"
+          />
+        </button>
+
+        <p className="text-base xl:mt-2">{project.name}</p>
       </div>
 
-      <p className="text-base xl:mt-2">{project.name}</p>
-    </div>
+      <ProjectModal data={project} open={open} onClose={() => setOpen(false)} />
+    </>
   );
 }
