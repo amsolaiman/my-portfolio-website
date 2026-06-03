f2bec5311eac0dcbd6f68a457d68a2e76dadfde6
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-06-03 15:42:22 +0800

    feat: create rich-text-renderer component

---
+++ src/components/rich-text-renderer/rich-text-renderer.tsx
@@ -0,0 +1,30 @@
+import {
+  PortableText,
+  PortableTextBlock,
+  PortableTextComponents,
+} from '@portabletext/react';
+
+// utils
+import { cn } from '@/utils/tw-merge';
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  value: PortableTextBlock[];
+  className?: string;
+};
+
+export default function RichTextRenderer({ value, className }: Props) {
+  const components: PortableTextComponents = {
+    block: {
+      normal: ({ children }) => (
+        <p className={cn('text-foreground text-xs', className)}>{children}</p>
+      ),
+    },
+    marks: {
+      em: ({ children }) => <span className="opacity-50">{children}</span>,
+    },
+  };
+
+  return <PortableText value={value} components={components} />;
+}
